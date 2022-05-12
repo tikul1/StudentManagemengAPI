051fba488e82549cd10ac93009aaf3767a341fae
Hardik Parmar <hardik.parmar5@gmail.com>
2022-05-12 17:55:51 +0530

    Swagger documentation on admin side

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const express = require("express");
+const swaggerUI = require("swagger-ui-express");
+const swaggerJsDoc = require("swagger-jsdoc");
+const YAML = require("yamljs");
 const mongoose = require("./db/db");
 const env = require("dotenv").config();
 const app = express();
@@ -12,3 +15,7 @@ const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log("app running on port 8080");
 });
+
+const options = YAML.load("studentManagement.yml");
+const specs = swaggerJsDoc(options);
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -3,11 +3,11 @@ const router = express.Router();
 const jwtHelper = require("../helpers/jwtHelper");
 const adminController = require("../controllers/adminController");
 router.get("/", adminController.adminList);
-router.get("/admin/:id", adminController.adminById);
+router.get("/searchadmin/:id", adminController.adminById);
 router.post("/addadmin", adminController.adminAdd);
 router.put("/updateadmin/:id", adminController.adminUpdate);
 router.delete("/removeadmin/:id", adminController.adminDelete);
-router.get("/login", adminController.adminLogin);
+router.post("/login", adminController.adminLogin);
 // router.get("/jwt", jwtHelper.verifyToken, teacherController.teacherList);
 
 module.exports = router;
