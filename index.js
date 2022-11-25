443d62c5c6c87cce621e64c19fa498469fe6266b
Hardik Parmar <hardik.parmar5@gmail.com>
2022-11-25 12:16:56 +0530

    using clustering in app to reduce load

--- index.js
+++ index.js
@@ -2,32 +2,56 @@ const express = require("express");
 const swaggerUI = require("swagger-ui-express");
 const swaggerJsDoc = require("swagger-jsdoc");
 const YAML = require("yamljs");
-
-const creaateHmac = require("crypto");
 const mongoose = require("./db/db");
 require("dotenv").config();
-const app = express();
-app.use(express.json());
+const cluster = require("cluster");
+const totalCPUs = require("os").cpus().length;
+// const app = express();
+// app.use(express.json());
 
-app.use("/admin", require("./routes/adminRoutes"));
-app.use("/teacher", require("./routes/teacherRoutes"));
-app.use("/student", require("./routes/studentRoutes"));
+if (cluster.isMaster) {
+  console.log(`Number of CPUs is ${totalCPUs}`);
+  console.log(`Master ${process.pid} is running`);
 
-// /*
-//       CRYPTO ENCRYPTION
-// */
-// const password = "Hardik@123";
-// // const cipher = crypto.createCipher("aes128", "hey");
-// const encrypted = createHmac("sha256", secret).update("I love cupcakes").digest("hex");
-// console.log({ encrypted });
-// // encrypted += cipher.final("hex");
-// console.log(encrypted);
+  // Fork workers.
+  for (let i = 0; i < totalCPUs; i++) {
+    cluster.fork();
+  }
 
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  console.log("app running on port 8080");
-});
+  cluster.on("exit", (worker, code, signal) => {
+    console.log(`worker ${worker.process.pid} died`);
+    console.log("Let's fork another worker!");
+    cluster.fork();
+  });
+} else {
+  const app = express();
+  app.use(express.json());
 
-const options = YAML.load("studentManagement.yml");
-const specs = swaggerJsDoc(options);
-app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
+  console.log(`Worker ${process.pid} started`);
+
+  app.use("/admin", require("./routes/adminRoutes"));
+  app.use("/teacher", require("./routes/teacherRoutes"));
+  app.use("/student", require("./routes/studentRoutes"));
+
+  const PORT = process.env.PORT || 8080;
+  app.listen(PORT, () => {
+    console.log("app running on port 8080");
+  });
+
+  const options = YAML.load("studentManagement.yml");
+  const specs = swaggerJsDoc(options);
+  app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
+}
+
+// app.use("/admin", require("./routes/adminRoutes"));
+// app.use("/teacher", require("./routes/teacherRoutes"));
+// app.use("/student", require("./routes/studentRoutes"));
+
+// const PORT = process.env.PORT || 8080;
+// app.listen(PORT, () => {
+//   console.log("app running on port 8080");
+// });
+
+// const options = YAML.load("studentManagement.yml");
+// const specs = swaggerJsDoc(options);
+// app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
